0ec476610c474fd1954e89cc528d53e3738f93bb
DATN-1 <hakaiosuke1557@gmail.com>
2026-03-30 00:26:57 +0700

    update orderType

--- src/app/type/orderType.ts
+++ src/app/type/orderType.ts
@@ -5,9 +5,9 @@ export interface Order extends RowDataPacket {
   id: number;
   user_id: number | null;
   order_code: string;
-  recipient_name: string;
-  recipient_phone: string;
-  shipping_address: string;
+  // recipient_name: string;
+  // recipient_phone: string;
+  // shipping_address: string;
   shipping_fee: number;
   total_amount: number;
   status: string;
